c3a0a67a43eb00e619b5b75f0fe435e5857bc721
Haastrup Daniel <140532908+hastrotech1@users.noreply.github.com>
2024-09-11 02:42:36 +0100

    Created the signup page with React + Vite, had minir issues but they are resolved *Except for the image alingnment*

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import ErrorPage from './components/errorPage/ErrorPage';
-import Login from './components/Login/Login';
-import Home from './components/Home';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import ErrorPage from "./components/errorPage/ErrorPage";
+import Login from "./components/Login/Login";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp/signup";
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='*' element={<ErrorPage />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
