8663aaaf6612bad720f6e53586437746558a3400
mosescode1 <emosescode@gmail.com>
2024-10-05 14:01:26 +0100

    Made changes to the profile Header for Dynamic Content

--- src/pages/components/ProfileHeader/ProfileHeader.tsx
+++ src/pages/components/ProfileHeader/ProfileHeader.tsx
@@ -1,30 +1,15 @@
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
-import userImage from "../../../assets/3.jpg";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import { NotificationsNoneOutlined } from "@mui/icons-material";
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import userImage from '../../../assets/3.jpg';
+import { NotificationsNoneOutlined } from '@mui/icons-material';
 
-const ProfileHeader = () => {
-  return (
-    <>
-      <header className="flex justify-between items-center ">
-        <h2 className="text-2xl font-semibold">Welcome, Aisha</h2>
-        <div className="flex items-center gap-7">
-          <NotificationsNoneOutlined className="scale-150 border rounded" />
-          <Stack>
-            <Avatar alt="profile image" src={userImage} />
-          </Stack>
-        </div>
-      </header>
-    </>
-  );
 const ProfileHeader = (prop) => {
 	return (
 		<>
 			<header className='flex justify-between items-center '>
 				{prop.content}
 				<div className='flex items-center gap-7'>
-					<NotificationsIcon className='scale-150 border rounded' />
+					<NotificationsNoneOutlined className='scale-150 border rounded' />
 					<Stack>
 						<Avatar alt='profile image' src={userImage} />
 					</Stack>
