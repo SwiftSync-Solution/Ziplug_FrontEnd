446ed96bb9e1367619ce034e2a43a41299523b1d
mosescode1 <emosescode@gmail.com>
2024-10-07 12:55:12 +0100

    created hep page and settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import ErrorPage from "./components/errorPage/ErrorPage";
-import Login from "./components/Login/Login";
-import Home from "./components/Home";
-import SignUp from "./components/SignUp/SignUp";
-import DashBoard from "./pages/components/DashBoard/Dashboard";
-import PlaceOrder from "./pages/components/PlaceOrder/PlaceOrder";
-import TrackShipment from "./pages/components/TrackShipment/trackShipment";
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/dashboard" element={<DashBoard />} />
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import ErrorPage from './components/errorPage/ErrorPage';
+import Login from './components/Login/Login';
+import Home from './components/Home';
+import SignUp from './components/SignUp/SignUp';
+import DashBoard from './pages/components/DashBoard/Dashboard';
+import PlaceOrder from './pages/components/PlaceOrder/PlaceOrder';
+import TrackShipment from './pages/components/TrackShipment/trackShipment';
+import Help from './pages/components/Help/Help';
+import Settings from './pages/components/SettingsComp/Settings';
 
-        <Route path="/place-order" element={<PlaceOrder />} />
-        <Route path="/track-shipment" element={<TrackShipment />} />
-        <Route path="/help" element={<DashBoard />} />
-        <Route path="/settings" element={<DashBoard />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </>
-  );
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<SignUp />} />
+				<Route path='/dashboard' element={<DashBoard />} />
+				<Route path='/place-order' element={<PlaceOrder />} />
+				<Route path='/track-shipment' element={<TrackShipment />} />
+				<Route path='/help' element={<Help />} />
+				<Route path='/settings' element={<Settings />} />
+				<Route path='*' element={<ErrorPage />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
--- src/pages/components/sideNav/SideBar.tsx
+++ src/pages/components/sideNav/SideBar.tsx
@@ -1,117 +1,112 @@
-import { Link } from "react-router-dom";
-import React from "react";
-import HomeIcon from "@mui/icons-material/Home";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import AddLocationIcon from "@mui/icons-material/AddLocation";
-import HelpIcon from "@mui/icons-material/Help";
-import SettingsIcon from "@mui/icons-material/Settings";
-import LogoutIcon from "@mui/icons-material/Logout";
-import { useState } from "react";
+import { Link } from 'react-router-dom';
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
+import HelpIcon from '@mui/icons-material/Help';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useState } from 'react';
 
 const SideBar: React.FC = () => {
-  return (
-    <>
-      <div className="flex h-screen">
-        {/* Sidebar */}
-        <aside className="bg-blue-900 w-64 text-white p-4 font-bold text-2xl">
-          <div className="h-[90vh] grid place-content-center">
-            {/* Sidebar Navigation */}
-            <NavLink />
-          </div>
+	return (
+		<>
+			<div className='flex h-screen'>
+				{/* Sidebar */}
+				<aside className='bg-blue-900 w-64 text-white p-4 font-bold text-2xl'>
+					<div className='h-[90vh] grid place-content-center'>
+						{/* Sidebar Navigation */}
+						<NavLink />
+					</div>
 
-          {/* Log Out Button */}
-          <div className="pl-4 hover:bg-white hover:text-black hover:rounded">
-            <Link to="/logout" className="flex items-center space-x-2">
-              <LogoutIcon />
-              <span>Log Out</span>
-            </Link>
-          </div>
-        </aside>
-      </div>
-    </>
-  );
+					{/* Log Out Button */}
+					<div className='pl-4 hover:bg-white hover:text-black hover:rounded'>
+						<Link to='/logout' className='flex items-center space-x-2'>
+							<LogoutIcon />
+							<span>Log Out</span>
+						</Link>
+					</div>
+				</aside>
+			</div>
+		</>
+	);
 };
 
 const NavLink = () => {
-  const [activeLink, setActiveLink] = useState<string | null>(null);
+	const [activeLink, setActiveLink] = useState<string | null>(null);
 
-  const handleLinkClick = (
-    link: string,
-    event: React.MouseEvent<HTMLAnchorElement>
-  ) => {
-    event.preventDefault(); // Prevent the default anchor click behavior
-    setActiveLink(link);
-  };
+	const handleLinkClick = (
+		link: string,
+		event: React.MouseEvent<HTMLAnchorElement>
+	) => {
+		event.preventDefault(); // Prevent the default anchor click behavior
+		setActiveLink(link);
+	};
 
-  return (
-    <nav className="space-y-4">
-      <div onClick={(event) => handleLinkClick("link1", event)}>
-        <Link
-          to="/dashboard"
-          className={`p-2 rounded-md ${
-            activeLink === "link1"
-              ? "flex items-center space-x-2 bg-white text-black rounded p-1"
-              : "flex items-center space-x-2"
-          }`}
-        >
-          <HomeIcon />
-          <span>Home</span>
-        </Link>
-      </div>
+	return (
+		<nav className='space-y-4'>
+			<div onClick={(event) => handleLinkClick('link1', event)}>
+				<Link
+					to='/dashboard'
+					className={`p-2 rounded-md ${
+						activeLink === 'link1'
+							? 'flex items-center space-x-2 bg-white text-black rounded p-1'
+							: 'flex items-center space-x-2  hover:bg-white hover:text-black hover:rounded'
+					}`}>
+					<HomeIcon />
+					<span>Home</span>
+				</Link>
+			</div>
 
-      <div onClick={(event) => handleLinkClick("link2", event)}>
-        <Link
-          to="/place-order"
-          className={`p-2 rounded-md ${
-            activeLink === "link2"
-              ? "flex items-center space-x-2 bg-white text-black rounded p-1"
-              : "flex items-center space-x-2"
-          }`}
-        >
-          <LocalShippingIcon />
-          <span>Place Order</span>
-        </Link>
-      </div>
+			<div onClick={(event) => handleLinkClick('link2', event)}>
+				<Link
+					to='/place-order'
+					className={`p-2 rounded-md ${
+						activeLink === 'link2'
+							? 'flex items-center space-x-2 bg-white text-black rounded p-1'
+							: 'flex items-center space-x-2  hover:bg-white hover:text-black hover:rounded'
+					}`}>
+					<LocalShippingIcon />
+					<span>Place Order</span>
+				</Link>
+			</div>
 
-      <div onClick={(event) => handleLinkClick("link3", event)}>
-        <Link
-          to="/track-shipment"
-          className={`p-2 rounded-md ${
-            activeLink === "link3"
-              ? "flex items-center space-x-2 bg-white text-black rounded p-1"
-              : "flex items-center space-x-2"
-          }`}
-        >
-          <AddLocationIcon />
-          <span>Track Shipment</span>
-        </Link>
-      </div>
+			<div onClick={(event) => handleLinkClick('link3', event)}>
+				<Link
+					to='/track-shipment'
+					className={`p-2 rounded-md ${
+						activeLink === 'link3'
+							? 'flex items-center space-x-2 bg-white text-black rounded p-1'
+							: 'flex items-center space-x-2  hover:bg-white hover:text-black hover:rounded'
+					}`}>
+					<AddLocationIcon />
+					<span>Track Shipment</span>
+				</Link>
+			</div>
 
-      <div onClick={(event) => handleLinkClick("link4", event)}>
-        <Link
-          to="/help"
-          className={`p-2 rounded-md ${
-            activeLink === "link4"
-              ? "flex items-center space-x-2 bg-white text-black rounded p-1"
-              : "flex items-center space-x-2"
-          }`}
-        >
-          <HelpIcon />
-          <span>Help</span>
-        </Link>
-      </div>
+			<div onClick={(event) => handleLinkClick('link4', event)}>
+				<Link
+					to='/help'
+					className={`p-2 rounded-md ${
+						activeLink === 'link4'
+							? 'flex items-center space-x-2 bg-white text-black rounded p-1'
+							: 'flex items-center space-x-2  hover:bg-white hover:text-black hover:rounded'
+					}`}>
+					<HelpIcon />
+					<span>Help</span>
+				</Link>
+			</div>
 
-      <div>
-        <Link
-          to="/settings"
-          className="flex items-center space-x-2 hover:bg-white hover:text-black hover:rounded p-1"
-        >
-          <SettingsIcon />
-          <span>Settings</span>
-        </Link>
-      </div>
-    </nav>
-  );
+			<div>
+				<Link
+					to='/settings'
+					className='flex items-center space-x-2 hover:bg-white hover:text-black hover:rounded p-1'>
+					<SettingsIcon />
+					<span>Settings</span>
+				</Link>
+			</div>
+		</nav>
+	);
 };
 
 export default SideBar;
